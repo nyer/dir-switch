21d4b407cc5e3fe9e14d546fcc19112129fe0b81
notifyer <ls5231@gmail.com>
2013-08-23 12:22:11 +0800

    prevent directory change

--- windows/mark.bat
+++ windows/mark.bat
@@ -1,4 +1,4 @@
-@echo off 
+@echo off
 set MARKPATH=D:\marks
 set TAR=%cd%
 if not exist %MARKPATH% (goto makedir) else (goto makelink)
@@ -9,6 +9,5 @@ attrib +h %MARKPATH%
 goto makelink
 
 :makelink
-cd %MARKPATH%
-echo %TAR% > %1
-cd %TAR%
+echo %TAR% > %MARKPATH%\%1
+cd /d %TAR%
